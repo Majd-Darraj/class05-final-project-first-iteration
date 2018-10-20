65bb39bc85c3a0ef47ab925db98122f4ccfc17c0
ibrsal <ibrsaldk@gmail.com>
2018-10-20 20:48:35 +0200

    adding homepage with header and search bar

---
+++ src/components/Homepage.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+import "../App.css";
+import {JobList } from "./componentsclass";
+
+class Homepage extends Component {
+
+  
+  render() {
+    return (
+      <div>
+      <div>
+        <Header />
+      </div>
+
+
+      
+      </div>
+    );
+  }
+}
+
+export default Homepage;
