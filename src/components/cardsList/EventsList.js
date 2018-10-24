58c12fbcb78af95468fea5ea7b12e94b6e567603
Milamanieva <milamanieva@gmail.com>
2018-10-24 10:04:54 +0200

    Events-Map is done

--- src/components/cardsList/EventsList.js
+++ src/components/cardsList/EventsList.js
@@ -2,53 +2,45 @@ import React from "react";
 //import { withScriptjs, withGoogleMap, GoogleMap } from "react-google-maps";
 import Header from "../Header";
 import EventCard from "../cards/EventCard";
+import MapComponent from "../map-component/MapComponent";
 
 class EventsList extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { eventsData: []
+    this.state = { 
+      eventsData: [],
+      coords: []
     };
   }
 
-  componentDidMount = () => {
-    this.fetchData();
-  };
 
+  componentDidMount() {
 
-  fetchData() {
+    let newCoords = [];
+
     fetch("https://raw.githubusercontent.com/paredesrichard/commandline/master/events.json"
     )
-      .then(response => response.json())
-      .then(parsedJSON =>
-        parsedJSON.map(event => ({
-          id: `${event.id}`,
-          event_name: `${event.event_name}`,
-          event_type: `${event.event_type}`,
-          event_address: `${event.event_address}`,
-          event_postal_code: `${event.event_postal_code}`,
-          event_city: `${event.event_city}`,
-          event_geo_lat: `${event.event_geo_lat}`,
-          event_geo_lng: `${event.event_geo_lng}`,
-          event_start_date:`${event.event_start_date}`,
-          event_end_date: `${event.event_end_date}`,
-          event_start_hour: `${event.event_start_hour}`,
-          event_end_hour: `${event.event_end_hour}`,
-          event_agenda: `${event.event_agenda}`,
-          event_language: `${event.event_language}`,
-          max_participants: `${event.max_participants}`,
-          event_theme_image: `${event.event_theme_image}`,
-          contact_person: `${event.contact_person}`,
-          contact_phone: `${event.contact_phone}`,
-          contact_email: `${event.contact_email}`,
-          active: `${event.active}`,
-          event_URL: `${event.event_URL}`
-        }))
-      )
+      .then(response => response.json())      
       .then(eventsData => {
-        this.setState({ eventsData });
+        this.setState({ eventsData: eventsData });
+      newCoords = eventsData.map(data => {
+        let tempCoords = {};
+        tempCoords = {
+          lat: data.event_geo_lat,
+          lng: data.event_geo_lng
+        };
+        return tempCoords;
       });
+      this.setState({ coords: newCoords });
+      });
+
   
-  }
+}
+
+
+
+
+
 
   render() {
     const { eventsData } = this.state;
@@ -113,6 +105,14 @@ class EventsList extends React.Component {
                 })
               : null}
           </div>
+          <div style={{ width: `400px`, height: `400px` }}>
+        <MapComponent
+        mapCenter={{ lat: 55.6802303, lng: 12.5718571 }}
+        setMarker
+        Zoom={11}
+        coords={this.state.coords}
+      />
+      </div>
          </section>
         </div>
     );
