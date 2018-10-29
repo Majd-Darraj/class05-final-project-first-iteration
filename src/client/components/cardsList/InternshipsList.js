3855c7561425128bdf2c8262bc47201a6fa3e575
ibrsal <ibrsaldk@gmail.com>
2018-10-29 21:36:02 +0100

    made server with local database

--- src/client/components/cardsList/InternshipsList.js
+++ src/client/components/cardsList/InternshipsList.js
@@ -17,41 +17,14 @@ class InternshipsList extends Component {
   };
 
   fetchData() {
-    fetch(
-      "https://raw.githubusercontent.com/paredesrichard/commandline/master/internships.json"
-    )
+    fetch("/api/internships")
+      // "https://raw.githubusercontent.com/paredesrichard/commandline/master/internships.json"
+    
       .then(response => response.json())
-      .then(parsedJSON =>
-        parsedJSON.map(internship => ({
-          id: `${internship.id}`,
-          internship_title: `${internship.internship_title}`,
-          internship_category: `${internship.internship_category}`,
-          department: `${internship.department}`,
-          internship_description: `${internship.internship_description}`,
-          organisation_description: `${internship.organisation_description}`,
-          internship_agreement: `${internship.internship_agreement}`,
-          internship_requirements: `${internship.internship_requirements}`,
-          application_requirements: `${internship.application_requirements}`,
-          internship_availabiltiy_schedule: `${
-            internship.internship_availabiltiy_schedule
-          }`,
-          travel_expenses: `${internship.travel_expenses}`,
-          location: `${internship.location}`,
-          internship_add_date: `${internship.internship_add_date}`,
-          closing_date: `${internship.closing_date}`,
-          internship_deadline: `${internship.internship_deadline}`,
-          contact_person: `${internship.contact_person}`,
-          phone_contact: `${internship.phone_contact}`,
-          email_contact: `${internship.email_contact}`,
-          organisation_address: `${internship.organisation_address}`,
-          organisation_website: `${internship.organisation_website}`,
-          internship_theme_image: `${internship.internship_theme_image}`,
-          active: `${internship.active}`
-        }))
-      )
+      
       .then(internshipsData =>
         this.setState({
-          internshipsData,
+          internshipsData: internshipsData,
           isLoading: false
         })
       )
@@ -60,71 +33,18 @@ class InternshipsList extends Component {
 
   render() {
     // JSON.stringify(this.state.InternshipData);
-    const { isLoading, internshipsData } = this.state;
+    const {  internshipsData ,isLoading } = this.state;
     return (
       <>
         <Header />
         <section
-          className={`cards-list-container ${isLoading ? "is-loading" : ""}`}
+          className={`cards-list-container `}
         >
           <div className="cards-list">
-            {!isLoading && internshipsData.length > 0
+            { internshipsData.length > 0
               ? internshipsData.map(internship => {
-                  const {
-                    id,
-                    internship_title,
-                    internship_category,
-                    organisation_name,
-                    department,
-                    internship_description,
-                    organisation_description,
-                    internship_agreement,
-                    internship_requirements,
-                    application_requirements,
-                    internship_availabiltiy_schedule,
-                    travel_expenses,
-                    location,
-                    internship_add_date,
-                    closing_date,
-                    internship_deadline,
-                    contact_person,
-                    phone_contact,
-                    email_contact,
-                    organisation_address,
-                    organisation_website,
-                    internship_theme_image,
-                    active
-                  } = internship;
-
-                  return (
-                    <InternshipCard
-                      internship_title={internship_title}
-                      id={id}
-                      key={id}
-                      department={department}
-                      internship_category={internship_category}
-                      organisation_name={organisation_name}
-                      internship_description={internship_description}
-                      organisation_description={organisation_description}
-                      internship_agreement={internship_agreement}
-                      internship_requirements={internship_requirements}
-                      application_requirements={application_requirements}
-                      internship_availabiltiy_schedule={
-                        internship_availabiltiy_schedule
-                      }
-                      travel_expenses={travel_expenses}
-                      location={location}
-                      internship_add_date={internship_add_date}
-                      closing_date={closing_date}
-                      internship_deadline={internship_deadline}
-                      contact_person={contact_person}
-                      phone_contact={phone_contact}
-                      email_contact={email_contact}
-                      organisation_address={organisation_address}
-                      organisation_website={organisation_website}
-                      internship_theme_image={internship_theme_image}
-                      active={active}
-                    />
+                    return (
+                    <InternshipCard  {...internship}    />
                   );
                 })
               : null}
