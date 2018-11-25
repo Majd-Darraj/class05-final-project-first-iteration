5b5959ec313ebcfcfdcb4876e553ae0044ce058c
Majd Darraj <majd.darraj@hotmail.com>
2018-11-25 14:32:13 +0100

    fix admin styling

--- src/server/config/db.js
+++ src/server/config/db.js
@@ -2,7 +2,7 @@ import mysql from "mysql2";
 
 const DATABASE_URL =
   // Mila DB (uncomment the next line)
-  // process.env.DATABASE_URL || "mysql://root:mila@127.0.0.1:3306/thread";
+  //  process.env.DATABASE_URL || "mysql://root:mila@127.0.0.1:3306/thread";
 
   // Majd DB (uncomment the next line)
   process.env.DATABASE_URL || "mysql://admin_m:hyfpass00@127.0.0.1:3306/thread";
